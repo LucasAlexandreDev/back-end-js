0a5f14afccad0e4040ba5baf935c338f43f7b0e8
Lucas Alexandre <lucasalexandre.lads13@gmail.com>
2026-01-29 19:55:17 -0300

    refactor: move app.js para pasta aula-01

--- aula-01/app.js
+++ aula-01/app.js
@@ -19,10 +19,8 @@ console.log(`O nome do usuário é: ${nome}`)
 
 console.log(nome + ' possui ' + idade + ' anos')
 
-const { endianness } = require('os')
 /* Importe da biblioteca readline
    readline -> Serve para permitir a entrada de dados via terminal */
-
 var readline = require('readline')
 
 // Cria um objeto especialista em entrada de dados pelo terminal 
@@ -46,11 +44,10 @@ entradaDeDados.question('Digite o seu nome: ',function(nomeUsuario){
 
     entradaDeDados.question('Digite o seu e-mail: ', function(emailUsuario){
         console.log(`O email do usuário ${nomeUsuario} é:  ${emailUsuario}`)
-    })
 
-    entradaDeDados.question('digite o seu número de telefone: ', function(telUsuario){
-        console.log(`O número de telefone do nome`)
+         entradaDeDados.question('digite o seu número de telefone: ', function(telUsuario){
+            console.log(`O número do seu telefone é: ${telUsuario}`)
+        })
     })
-
 })
 
